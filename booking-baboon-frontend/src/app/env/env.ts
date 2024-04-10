f10aeab8980f65ebe6ef9b4ba1d5c8d9f87a83af
Ilija <ilija.beslin@gmail.com>
2024-04-10 20:41:24 +0200

    feat: changed protocol to https

--- booking-baboon-frontend/src/app/env/env.ts
+++ booking-baboon-frontend/src/app/env/env.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiHost: 'http://localhost:8080/api/v1/',
-  socketHost: 'http://localhost:8080/'
+  apiHost: 'https://localhost:8080/api/v1/',
+  socketHost: 'https://localhost:8080/'
 }
